628d7337760814dd4b8a52ef54328e218ec94cd1
Ethan <stayhungry134@gmail.com>
2023-09-10 17:32:12 +0800

    feat: 第一章，引论

---
+++ src/chapter1/chapter_1.h
@@ -0,0 +1,11 @@
+//
+// Created by stayh on 2023/9/9.
+//
+
+#ifndef DATA_STRUCTURE_CHAPTER_1_H
+#define DATA_STRUCTURE_CHAPTER_1_H
+
+#endif //DATA_STRUCTURE_CHAPTER_1_H
+
+// 计算程序执行时间
+extern void time_test();
---
+++ src/chapter1/time_test.c
@@ -0,0 +1,22 @@
+﻿//
+// Created by stayh on 2023/9/10.
+//
+#include <time.h>
+
+void time_test() {
+    struct timeval start, end;
+    long seconds, microseconds;
+
+    gettimeofday(&start, NULL); // 记录开始时间
+
+    // 在这里放置要测量执行时间的代码
+    printf("%d\n", 1654151 / 2);
+
+    gettimeofday(&end, NULL); // 记录结束时间
+
+    seconds = end.tv_sec - start.tv_sec;
+    microseconds = end.tv_usec - start.tv_usec;
+    double elapsed = seconds + microseconds / 1000000.0;
+
+    printf("程序执行时间：%f 秒\n", elapsed);
+}
