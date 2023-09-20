93b6ba894e636487242f298601ac0b3fdb0a6993
Ethan <stayhungry134@gmail.com>
2023-09-21 00:26:12 +0800

    feat: 使用数组实现一个栈

---
+++ src/chapter3/stack_list.c
@@ -0,0 +1,88 @@
+﻿//
+// Created by stayh on 2023/9/20.
+//
+#include "stack_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// 定义一种错误
+void FatalError(const char *message) {
+    fprintf(stderr, "Fatal Error: %s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+// 判断是否为空
+int is_empty(Stack S) {
+    return S->TopOfStack == EmptyTOS;
+}
+
+// 判断是否满了
+int is_full(Stack S) {
+    return S->TopOfStack == S->Capacity - 1;
+}
+
+Stack create_stack(int Max_Elements) {
+    Stack S;
+    if (Max_Elements < MinStackSize) {
+        FatalError("Stack size is too small");
+    }
+    S = malloc(sizeof(struct StackRecord));
+    if (S == NULL) {
+        FatalError("Out of space!!!");
+    }
+    S->Array = malloc(sizeof(ElementType) * Max_Elements);
+    if (S->Array == NULL) {
+        FatalError("Out of space!!!");
+    }
+    S->Capacity = Max_Elements;
+    make_empty(S);
+
+    return S;
+}
+
+// 释放一个栈
+void dispose_stack(Stack S) {
+    if (S != NULL) {
+        free(S->Array);
+        free(S);
+    }
+}
+
+// 栈置空
+void make_empty(Stack S) {
+    S->TopOfStack = EmptyTOS;
+}
+
+// push 一个元素
+void push(ElementType X, Stack S) {
+    if (is_full(S)) {
+        FatalError("Full stack");
+    } else {
+        S->Array[++S->TopOfStack] = X;
+    }
+}
+
+// 返回栈的 top
+ElementType top(Stack S) {
+    if (!is_empty(S)) {
+        return S->Array[S->TopOfStack];
+    }
+    FatalError("Empty stack");
+}
+
+// pop 一个元素
+void pop(Stack S) {
+    if (is_empty(S)) {
+        FatalError("Empty stack");
+    } else {
+        S->TopOfStack--;
+    }
+}
+
+// 返回栈的 top 并且 pop
+ElementType top_and_pop(Stack S) {
+    if (!is_empty(S)) {
+        return S->Array[S->TopOfStack--];
+    }
+    FatalError("Empty stack");
+}
---
+++ src/chapter3/stack_list.h
@@ -0,0 +1,45 @@
+﻿//
+// Created by stayh on 2023/9/20.
+//
+/// 使用数组实现一个栈，如果 TopOfStack 为 -1 则为空
+#ifndef DATA_STRUCTURE_STACK_LIST_H
+#define DATA_STRUCTURE_STACK_LIST_H
+struct StackRecord;
+typedef struct StackRecord * Stack;
+
+typedef int ElementType;
+
+
+// 判断是否为空
+int is_empty(Stack S);
+// 判断是否满了
+int is_full(Stack S);
+// 创建一个栈
+Stack create_stack(int MaxElements);
+// 释放一个栈
+void dispose_stack(Stack S);
+// 栈置空
+void make_empty(Stack S);
+// push 一个元素
+void push(ElementType X, Stack S);
+// 返回栈的 top
+ElementType top(Stack S);
+// pop 一个元素
+void pop(Stack S);
+// 返回栈的 top 并且 pop
+ElementType top_and_pop(Stack S);
+
+#endif //DATA_STRUCTURE_STACK_LIST_H
+//
+#define EmptyTOS (-1)
+//
+#define MinStackSize (5)
+
+struct StackRecord{
+    // 栈的容量
+    int Capacity;
+    // 栈顶
+    int TopOfStack;
+    // 栈的数组
+    ElementType *Array;
+};
