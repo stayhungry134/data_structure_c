7e32c0e8e2f7ff45d1219c96c6b0fa2f49c239bb
Ethan <stayhungry134@gmail.com>
2023-09-21 00:25:54 +0800

    feat: 使用链表实现一个栈

---
+++ src/chapter3/stack.c
@@ -0,0 +1,71 @@
+﻿//
+// Created by stayh on 2023/9/20.
+//
+#include "stack.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+// 定义一种错误
+void FatalError(const char *message) {
+    fprintf(stderr, "Fatal Error: %s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+// 判断是否为空
+int is_empty(Stack S){
+    return S->Next == NULL;
+}
+
+// 创建一个栈
+Stack create_stack(void){
+    Stack S;
+    S = malloc(sizeof (struct StackNode));
+    if (S == NULL){
+        FatalError("Out of space!!!");
+    }
+    S->Next = NULL;
+    make_empty(S);
+    return S;
+}
+
+// 置空一个栈
+void make_empty(Stack S){
+    if (S == NULL){
+        FatalError("Must use CreateStack first");
+    } else{
+        while(!is_empty(S))
+            pop(S);
+    }
+}
+
+// push 一个元素
+void push(ElementType X, Stack S){
+    PtrToNode tem_cell;
+    tem_cell = malloc(sizeof (struct StackNode));
+    if (tem_cell == NULL)
+        FatalError("Out of space!!!");
+    else{
+        tem_cell->Element = X;
+        tem_cell->Next = S->Next;
+        S->Next = tem_cell;
+    }
+}
+
+// 返回头元素
+ElementType top(Stack S){
+    if (!is_empty(S))
+        return S->Next->Element;
+    FatalError("Empty stack");
+}
+
+// pop 一个元素
+void pop(Stack S){
+    PtrToNode first_cell;
+    if (is_empty(S))
+        FatalError("Empty stack");
+    else{
+        first_cell = S->Next;
+        S->Next = S->Next->Next;
+        free(first_cell);
+    }
+}
---
+++ src/chapter3/stack.h
@@ -0,0 +1,33 @@
+﻿//
+// Created by stayh on 2023/9/20.
+//
+/// 实现一个栈。可以使用数组实现栈，也可以使用指针实现栈，如果使用好的编程原则，那么调用例程不必知道使用的是哪种方式
+/// 这里使用链表实现一个栈
+#ifndef DATA_STRUCTURE_STACK_H
+#define DATA_STRUCTURE_STACK_H
+struct StackNode;
+typedef struct StackNode * PtrToNode;
+typedef PtrToNode Stack;
+typedef int ElementType;
+
+
+// 判断是否为空
+int is_empty(Stack S);
+// 创建一个栈
+Stack create_stack(void);
+//
+void dispose_stack(Stack S);
+// 栈置空
+void make_empty(Stack S);
+// push 一个元素
+void push(ElementType X, Stack S);
+// 返回栈的 top
+ElementType top(Stack S);
+// pop 一个元素
+void pop(Stack S);
+#endif //DATA_STRUCTURE_STACK_H
+
+struct StackNode{
+    ElementType Element;
+    PtrToNode Next;
+};
