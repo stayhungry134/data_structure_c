a5cc31334df360c075ed845305a698df64fb1a9d
Ethan <stayhungry134@gmail.com>
2023-09-27 17:29:00 +0800

    feat: 链表 leetcode练习

---
+++ src/chapter3/leetcode/环形链表.c
@@ -0,0 +1,55 @@
+﻿//
+// Created by Ethan on 2023/9/27.
+//
+#include "stdbool.h"
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+/**
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+ * 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ */
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+bool hasCycle(struct ListNode *head) {
+    if (!head){
+        return false;
+    }
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
+    while (fast->next && fast->next->next){
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow){
+            return true;
+        }
+    }
+    return false;
+}
+
+/// 见到一个 0ms 的解法，算是钻了题目的空子，
+/// 遍历到的时候把节点加一个大于题目给定范围的数，后续的遍历中把节点的值和这个大范围的数做比较，
+/// 如果超过这个数，就说明之前来过，因为原始的数不会有这么大，只有可能是之前来过，然后加上了一个数
+bool hasCycle1(struct ListNode *head) {
+    int large = 100001;
+    while (head){
+        if (head->val > large){
+            return true;
+        }
+        head->val += large;
+        head = head->next;
+    }
+    return false;
+}
---
+++ src/chapter3/leetcode/相交链表.c
@@ -0,0 +1,79 @@
+﻿//
+// Created by Ethan on 2023/9/27.
+//
+#include "stdio.h"
+
+/// 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    typedef struct ListNode * position;
+    // 先找到尾结点，如果尾结点是一样的，那么就相交
+    position tail_headA = headA;
+    position tail_headB = headB;
+    int len_a = 0, len_b = 0;
+    while (tail_headA->next){
+        tail_headA = tail_headA->next;
+        len_a += 1;
+    }
+    while (tail_headB->next){
+        tail_headB = tail_headB->next;
+        len_b += 1;
+    }
+    if (tail_headA != tail_headB){
+        return NULL;
+    }
+    // 计算两个长度之间的差值
+    int diff_count = 0;
+    position tem_a = headA, tem_b = headB;
+    if (len_b > len_a){
+        diff_count = len_b -len_a;
+        while (diff_count > 0){
+            tem_b = tem_b->next;
+            diff_count -= 1;
+        }
+    } else{
+        diff_count = len_a -len_b;
+        while (diff_count > 0){
+            tem_a = tem_a->next;
+            diff_count -= 1;
+        }
+    }
+    while (tem_a != tem_b){
+        tem_a = tem_a->next;
+        tem_b = tem_b->next;
+    }
+    return tem_b;
+}
+
+/**
+ * 更简单的方法就是 两条链表一起遍历，当其中一条到达终点的时候，说明这一条是短的，可以让这一条短的重新指向另一个链表的开头
+ * 当另一个比较长的链表到达链表尾的时候，就把它指向较短的那个链表，这样的话就能够保证这时候两个链表距离终点的位置是相同的。
+ *
+ */
+
+struct ListNode *getIntersectionNode1(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL) {
+        return NULL;
+    }
+    typedef struct ListNode *position;
+    position a = headA, b = headB;
+    while (a != b){
+        if (!a){
+            a = headB;
+        } else{
+            a=a->next;
+        }
+        if (!b){
+            b = headA;
+        } else{
+            b=b->next;
+        }
+    }
+    return a;
+}
