ecb3c578a95aaf5987d14925fce71f85beae3159
Ethan <stayhungry134@gmail.com>
2023-09-28 16:27:12 +0800

    feat: 使用数组实现一个栈

---
+++ src/chapter3/queue.c
@@ -0,0 +1,106 @@
+﻿//
+// Created by Ethan on 2023/9/22.
+//
+#include "queue.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+// 判断队列是否为空
+int is_empty(Queue Q){
+    return Q->Size == 0;
+}
+
+// 判断队列是否已满
+int is_full(Queue Q){
+    return Q->Size == Q->Capacity;
+}
+
+// 释放一个队列
+void dispose_queue(Queue Q){
+    if(Q != NULL){
+        free(Q->Array);
+        free(Q);
+    }
+}
+
+// 创建一个队列
+Queue create_queue(int MaxElements){
+    Queue Q;
+    if(MaxElements < MinQueueSize){
+        printf("Queue size is too small");
+        exit(1);
+    }
+    Q = malloc(sizeof(struct QueueRecord));
+    if(Q == NULL){
+        printf("Out of space");
+        exit(1);
+    }
+    Q->Array = malloc(sizeof(ElementType) * MaxElements);
+    if(Q->Array == NULL){
+        printf("Out of space");
+        exit(1);
+    }
+    Q->Capacity = MaxElements;
+    make_empty(Q);
+    return Q;
+}
+
+
+// 构造空队列
+void make_empty(Queue Q){
+    Q->Size = 0;
+    Q->Front = 1;
+    Q->Rear = 0;
+}
+
+static int succ(int Value, Queue Q){
+    if (++Value == Q->Capacity){
+        Value = 0;
+    }
+    return Value;
+}
+// 入队
+void enqueue(ElementType X, Queue Q){
+    if(is_full(Q)){
+        printf("Full queue");
+        exit(1);
+    }else{
+        Q->Size++;
+        Q->Rear = succ(Q->Rear, Q);
+        Q->Array[Q->Rear] = X;
+    }
+}
+
+// 返回队列的头
+ElementType front(Queue Q){
+    if(!is_empty(Q)){
+        return Q->Array[Q->Front];
+    }
+    printf("Empty queue");
+    exit(1);
+}
+
+// 出队
+void dequeue(Queue Q){
+    if(is_empty(Q)){
+        printf("Empty queue");
+        exit(1);
+    }else{
+        Q->Size--;
+        Q->Front = succ(Q->Front, Q);
+    }
+}
+
+// 返回队列的头并出队
+ElementType front_and_dequeue(Queue Q){
+    ElementType X = 0;
+    if(is_empty(Q)){
+        printf("Empty queue");
+        exit(1);
+    }else{
+        Q->Size--;
+        X = Q->Array[Q->Front];
+        Q->Front = succ(Q->Front, Q);
+    }
+    return X;
+}
---
+++ src/chapter3/queue.h
@@ -0,0 +1,31 @@
+﻿//
+// Created by Ethan on 2023/9/22.
+//
+/// 使用数组实现一个队列
+#ifndef DATA_STRUCTURE_QUEUE_H
+#define DATA_STRUCTURE_QUEUE_H
+struct QueueRecord;
+typedef struct QueueRecord * Queue;
+
+typedef int ElementType;
+
+int is_empty(Queue Q);
+int is_full(Queue Q);
+Queue  create_queue(int MaxElements);
+void dispose_queue(Queue Q);
+void make_empty(Queue Q);
+void enqueue(ElementType X, Queue Q);
+ElementType front(Queue Q);
+void dequeue(Queue Q);
+ElementType front_and_dequeue(Queue Q);
+#endif //DATA_STRUCTURE_QUEUE_H
+
+#define  MinQueueSize (5)
+
+struct QueueRecord{
+    int Capacity;
+    int Front;
+    int Rear;
+    int Size;
+    ElementType *Array;
+};
