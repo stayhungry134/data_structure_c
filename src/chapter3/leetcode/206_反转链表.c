dd6a4833b9bac8d0fcd37e2d511a1526c34cae6c
Ethan <stayhungry134@gmail.com>
2023-10-10 14:47:09 +0800

    feat: 反转链表

---
+++ src/chapter3/leetcode/206_反转链表.c
@@ -0,0 +1,27 @@
+﻿//
+// Created by Ethan on 2023/9/28.
+//
+/// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    // 如果链表为空或者只有一个节点
+    if (!head || !head->next){
+        return head;
+    }
+    struct ListNode * pre_cell;
+    struct ListNode * next_cell;
+    struct ListNode * position = head;
+    while (position){
+        pre_cell = position;
+        next_cell = position->next;
+        position = next_cell;
+        next_cell->next = pre_cell;
+    }
+    return pre_cell;
+}
