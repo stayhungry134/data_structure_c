b192de952a10744c3eb309c9bae6d96f94afa7cf
Ethan <stayhungry134@gmail.com>
2024-03-05 09:56:50 +0800

    style: 栈注释调整

--- src/chapter3/stack_list.h
+++ src/chapter3/stack_list.h
@@ -9,37 +9,22 @@ typedef struct StackRecord * Stack;
 
 typedef int ElementType;
 
-
-// 判断是否为空
-int is_empty(Stack S);
-// 判断是否满了
-int is_full(Stack S);
-// 创建一个栈
-Stack create_stack(int MaxElements);
-// 释放一个栈
-void dispose_stack(Stack S);
-// 栈置空
-void make_empty(Stack S);
-// push 一个元素
-void push(ElementType X, Stack S);
-// 返回栈的 top
-ElementType top(Stack S);
-// pop 一个元素
-void pop(Stack S);
-// 返回栈的 top 并且 pop
-ElementType top_and_pop(Stack S);
-
+int is_empty(Stack S);  // 判断是否为空
+int is_full(Stack S);   // 判断是否满了
+Stack create_stack(int MaxElements);    // 创建一个栈
+void dispose_stack(Stack S);    // 释放一个栈
+void make_empty(Stack S);   // 栈置空
+void push(ElementType X, Stack S);  // push 一个元素
+ElementType top(Stack S);   // 返回栈的 top
+void pop(Stack S);  // pop 一个元素
+ElementType top_and_pop(Stack S);   // 返回栈的 top 并且 pop
 #endif //DATA_STRUCTURE_STACK_LIST_H
-//
+
 #define EmptyTOS (-1)
-//
 #define MinStackSize (5)
 
 struct StackRecord{
-    // 栈的容量
-    int Capacity;
-    // 栈顶
-    int TopOfStack;
-    // 栈的数组
-    ElementType *Array;
+    int Capacity;   // 栈的容量
+    int TopOfStack; // 栈顶
+    ElementType *Array; // 栈的数组
 };
